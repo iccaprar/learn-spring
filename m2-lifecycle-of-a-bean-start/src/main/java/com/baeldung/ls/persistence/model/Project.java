42fd9daacd30c4af056097298e9e03b91c939410
Cristian Caprar <ccaprar@gmail.com>
2020-07-16 22:57:16 +0300

    setup lesson 4

--- m2-lifecycle-of-a-bean-start/src/main/java/com/baeldung/ls/persistence/model/Project.java
+++ m2-lifecycle-of-a-bean-start/src/main/java/com/baeldung/ls/persistence/model/Project.java
@@ -1,9 +1,18 @@
 package com.baeldung.ls.persistence.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalDate;
 import java.util.Objects;
 import java.util.Random;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class Project {
 
     private Long id;
@@ -22,79 +31,11 @@ public class Project {
     }
 
     public Project(String name, LocalDate dateCreated) {
-        this.id = new Random().nextLong();
-        this.name = name;
-        this.dateCreated = dateCreated;
+        this(null, name, dateCreated);
     }
 
     public Project(Project project) {
         this(project.getId(), project.getName(), project.getDateCreated());
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(LocalDate dateCreated) {
-        this.dateCreated = dateCreated;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((dateCreated == null) ? 0 : dateCreated.hashCode());
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Project other = (Project) obj;
-        if (dateCreated == null) {
-            if (other.dateCreated != null)
-                return false;
-        } else if (!dateCreated.equals(other.dateCreated))
-            return false;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Project [id=" + id + ", name=" + name + "] \n";
     }
 
 }
